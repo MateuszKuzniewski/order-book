666d5b5824427011d89029cd05eb530f86d17855
MateuszKuzniewski <kuzniewskii.mateusz@gmail.com>
2026-02-17 20:55:15 +0000

    removed overloaded function

--- src/utility.cpp
+++ src/utility.cpp
@@ -1,7 +1,7 @@
 #include <iostream>
 //------------------
 #include "utility.h"
-
+#include "orderbook.h"
 
 void PrintDeque(const std::deque<Order>& d)
 {
@@ -11,7 +11,7 @@ void PrintDeque(const std::deque<Order>& d)
     }
 };
 
-void PrintMap(const std::map<u64, std::deque<Order>, std::greater<u64>>& map, const std::string& text)
+void PrintMap(const OrderMap& map, const std::string& text)
 {
     std::cout << text << "\n\n";
     for (auto it = map.begin(); it != map.end(); ++it)
@@ -22,18 +22,3 @@ void PrintMap(const std::map<u64, std::deque<Order>, std::greater<u64>>& map, co
     } 
     std::cout << "\n";
 };
-
-void PrintMap(const std::map<u64, std::deque<Order>, std::less<u64>>& map, const std::string& text)
-{
-    std::cout << text << "\n\n";
-    for (auto it = map.begin(); it != map.end(); ++it)
-    {
-        const auto& [price, queue] = *it;
-        std::cout << "price: " << price << " Order: "; 
-        PrintDeque(queue);
-    } 
-    std::cout << "\n";
-};
-
-
-
