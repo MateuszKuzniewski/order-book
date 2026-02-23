49a1744d5d32041abe1e425d3e953b892523d232
MateuszKuzniewski <kuzniewskii.mateusz@gmail.com>
2026-02-23 20:42:08 +0000

    added data structure to hold orderlocation for order cancellation

--- src/orderbook.h
+++ src/orderbook.h
@@ -1,9 +1,9 @@
 #pragma once
 #include <deque>
 #include <map>
+#include <unordered_map>
 //----------------
 #include "types.h"
-
 
 enum class Side : u8
 {
@@ -19,11 +19,25 @@ struct Order
     Side side;
 };
 
-// mapping: price, list of orders for that price
-typedef std::map<u32, std::deque<Order>> OrderMap;
+typedef std::deque<Order> OrderList;
+
+// mapping: price -> list of orders for that price
+typedef std::map<u32, OrderList> OrderMap;
+
+
+struct OrderLocation
+{
+    OrderMap::iterator  priceLevelIterator;
+    OrderList::iterator orderIterator;
+    Side side;
+};
+
+// mapping: order id -> order location
+typedef std::unordered_map<u64, OrderLocation> OrderIndex;
 
 struct OrderBook
 {
     OrderMap bidBook;
     OrderMap askBook;
+    OrderIndex orderIndex;
 };
